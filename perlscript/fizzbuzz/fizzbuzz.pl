cd224b9c722320624708c396cfb49c05cdaa0e3d
AzureStone <azurestone@azurestone.net>
2009-12-07 02:37:53 +0900

    ファイル整理もかねて動かなくなっていたスクリプトと直しておいた。動かないスクリプトを公開リポジトリにあげておくのは気持ち悪い。

--- perlscript/fizzbuzz/fizzbuzz.pl
+++ perlscript/fizzbuzz/fizzbuzz.pl
@@ -1,9 +1,20 @@
 #!/usr/bin/perl -l
 
-for ($i=1; $i<=100; $i++) {
-	print "$i FizzBuzz"; if     ($i%3 == 0 && $i%5 == 0 )
-	print "$i Fizz";     elsif  ($i%3 == 0)
-	print "$i Buzz";     elsif  ($i%5 == 0)
-	print "$i";          else
+use strict;
+use warnings;
+
+#for (my $i=1; $i<=100; $i++) {
+#	print "$i FizzBuzz"; if     ($i%3 == 0 && $i%5 == 0 )
+#  print "$i Fizz";     elsif  ($i%3 == 0)
+#	print "$i Buzz";     elsif  ($i%5 == 0)
+#	print "$i";          else
+#}
+
+foreach (1..100) {
+  if ($_%3 == 0 && $_%5 == 0) { print "$_ FizzBuzz"; }
+  elsif ($_%3 == 0)           { print "$_ Fizz"; }
+  elsif ($_%5 == 0)           { print "$_ Buzz"; }
+  else                        { print "$_"; }
 }
 
+1;
