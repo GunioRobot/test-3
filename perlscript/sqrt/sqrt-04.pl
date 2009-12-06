66cf7a517ff2b09bda120ba48837dec69114d12a
AzureStone <azurestone@azurestone.net>
2009-12-07 00:56:25 +0900

    スローンのオンライン整数列大辞典にコピペしやすいようにした

---
+++ perlscript/sqrt/sqrt-04.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %input_num;
+
+$input_num{'start'}  = $ARGV[0];
+$input_num{'end'}    = $ARGV[1];
+my $count;
+
+foreach ( $input_num{'start'}..$input_num{'end'} ) {
+  if (sqrt($_) =~ /^\d+$/) {
+    print $_.',';
+    ++$count;
+  }
+}
+
+print "\n";
+
+foreach ( $input_num{'start'}..$input_num{'end'} ) {
+  if (sqrt($_) =~ /^\d+$/) {
+    print sqrt($_).',';
+    ++$count;
+  }
+}
+
+print "\n---------------------\n";
+print "$input_num{'start'} ～ $input_num{'end'}\n";
+print "整数は、$count個です。\n";
+
+1;
