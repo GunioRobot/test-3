af7b40fc1b49247927f4b9a78b3bf24bea4f3180
AzureStone <azurestone@azurestone.net>
2009-12-07 14:31:43 +0900

    昼飯中。あのときのコードを思い出しながらコーディング

---
+++ c/mystery-code/mystery-code-02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+  char mojiretsu;
+  mojiretsu = "A";
+  printf("%c\n",mojiretsu);
+
+
+  return 1;
+
+}
+
---
+++ c/mystery-code/mystery-code-03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+  char mojiretsu[100];
+  mojiretsu = 'AzureStone';
+  printf("%c\n",mojiretsu);
+
+
+  return 1;
+
+}
+
