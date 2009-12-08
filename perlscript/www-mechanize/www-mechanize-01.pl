a30e54171b80415ea1dc5386d0fa53bc7d5dbc6c
AzureStone <azurestone@azurestone.net>
2009-12-09 00:00:34 +0900

    久しぶりにmechanize

--- perlscript/www-mechanize/www-mechanize-01.pl
+++ perlscript/www-mechanize/www-mechanize-01.pl
@@ -10,4 +10,13 @@ my $mech  = new WWW::Mechanize( autocheck=>1 );
 
 $mech->get('http://www.research.att.com/~njas/sequences/index.html?language=japanese');
 
+$mech->submit_form(
+  fields => {
+    q => '1,4,9,16,25,36,49,64,81,100,',
+    language=>'japanese',
+    go=> '検索',
+    },
+);
+
+print Dumper $mech;
 1;
