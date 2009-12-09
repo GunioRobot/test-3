eb4ff2731a20448954cc96e76f8c4fce2dd33844
AzureStone <azurestone@azurestone.net>
2009-12-09 14:51:02 +0900

    食事休憩中：思案中

---
+++ perlscript/firefox/firefox-02.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use WWW::Mechanize;
+use YAML::Tiny;
+
+my %input_num;
+my $result_numbers = '';
+
+$input_num{'start'}  = $ARGV[0]; 
+$input_num{'end'}    = $ARGV[1];
+
+foreach ( $input_num{'start'}..$input_num{'end'} ) { 
+  if (sqrt($_) =~ /^\d+$/) {
+    $result_numbers = $_.',';
+  }
+  #ここを考える 
+}
+
+print $result_numbers . "\n";
+
+my $yaml  = YAML::Tiny->new;
+$yaml     = YAML::Tiny->read('config.yaml');
+
+my $mech  = new WWW::Mechanize( autocheck=>1 );
+
+$mech->get( $yaml->[0]->{sloan}->{url} );
+
+$mech->submit_form(
+  fields => {
+    q         =>  $result_numbers,
+    language  =>  'japanese',
+    go        =>  '検索',
+  },
+);
+
+print $mech->{last_uri} . "\n";
+
+1;
