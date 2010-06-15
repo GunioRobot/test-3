5272ffd7799ce767329992cf6f6daef2235253aa
AzureStone <azurestone@azurestone.net>
2010-06-16 03:02:03 +0900

    orz.......

---
+++ perlscript/yaml-question/04.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use YAML;
+
+my $yaml = YAML::LoadFile('./conf.d/01.yml');
+
+print Dumper $yaml;
+
+print "$yaml->{fruit}->[0]\n";
+
+foreach (@{$yaml->{fruit}}) {
+  print "$_\n";
+}
+
+
+
+sub DESTROY {
+  print "destory\n";
+}
+
+
+
+
+
