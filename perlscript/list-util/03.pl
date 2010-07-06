240ba082592dac76049795c6a64c1cd390937399
AzureStone <azurestone@azurestone.net>
2010-07-06 20:04:58 +0900

    List-Util test

---
+++ perlscript/list-util/03.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use List::Util qw(min);
+
+
+my %hash = (
+  12 => '1354',
+  193 => '6543',
+);
+
+my @vector = values (%hash);
+
+print Dumper @vector;
+
+
+my $test = min @vector;
+
+print "$test\n";
+
+
+
+exit(1);
+
+
+
