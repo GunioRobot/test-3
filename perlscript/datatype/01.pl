ee9a1726ebf67ec09374c00b82cfa146b93fb08b
AzureStone <azurestone@azurestone.net>
2010-07-13 17:40:31 +0900

    supervisor funny...

---
+++ perlscript/datatype/01.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+@int_var = (0,1,2,3,4,5);
+@char_var = ("0","1","2","3","4","5");
+
+
+printf("---------------------------------\n");
+
+  if ($int_var[0] == 0) {
+    print "this is a 0\n";
+  }
+
+  if ($int_var[0] == 1) {
+    print "this is a 1\n";
+  }
+
+printf("---------------------------------\n");
+
+  if ($char_var[0] == 0) {
+    print "this is a 0\n";
+  }
+
+  if ($char_var[0] == 1) {
+    print "this is a 1\n";
+  }
+
+printf("---------------------------------\n");
+
+exit(1);
+
---
+++ perlscript/dobule/01.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use YAML qw(LoadFile DumpFile);
+
+use 
+
+
